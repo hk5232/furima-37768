504257066d2f70ffd94f127f503f2dbe6217158d
hk5232 <kazamu132@gmail.com>
2022-03-30 10:59:16 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  before_action :item_set, only: [:show, :edit, :update]
+  before_action :item_set, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -37,6 +37,14 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    if current_user == @item.user
+    @item.destroy
+    redirect_to root_path
+    end
+  end
+
 
   private
 
